659f3e4c5e8201fb4bf1305ff541e3091c4121a3
zhangjiqiang <531560278@qq.com>
2018-11-21 14:48:14 +0800

    Initial commit

---
+++ src/main/java/com/yang/dior/DiorApplication.java
@@ -0,0 +1,12 @@
+package com.yang.dior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiorApplication.class, args);
+    }
+}
